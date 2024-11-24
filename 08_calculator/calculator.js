c241ce4141ee4a665948bc3fd53821b4390640b6
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-11-24 17:23:32 +0200

    calculator solution complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  let sum = 0;
+	numArray.forEach((value) => {
+    sum += value;
+  })
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+  let num = numArray[0] * numArray[1];
+  numArray.forEach((value, index) => {
+    if (index === 0 || index === 1) {
 
+    }
+    else {
+      num *= value;
+    }
+  })
+
+  return num;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let result = 1;
+  for (i = num1; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
