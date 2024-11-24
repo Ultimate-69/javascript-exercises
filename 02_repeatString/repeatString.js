d9c48458696d6c63e598ade4a8994dac48c5075c
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-11-24 16:10:55 +0200

    repeatString solution complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, amount) {
+    textReturn = "";
+    if (amount < 0) {
+        return 'ERROR';
+    }
 
+    for (i = 0; i < amount; i++) {
+        textReturn += text;
+    }
+
+    return textReturn
 };
 
 // Do not edit below this line
