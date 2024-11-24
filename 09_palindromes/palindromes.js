41bdf355b91f5d372cb81252a373a45880a811fe
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-11-24 17:36:25 +0200

    palindromes solution complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (stringText) {
+    stringText = stringText.replace(/,/g, '');
+    stringText = stringText.replace(/ /g, '');
+    stringText = stringText.replace(/\./g, '');
+    stringText = stringText.replace(/!/g, '');
 
+    let reversedText = ''
+
+    for (i = stringText.length - 1; i >= 0; i--) {
+        reversedText += stringText[i];
+    }
+
+    console.log(reversedText.toLowerCase());
+    console.log(stringText.toLowerCase());
+    return reversedText.toLowerCase() == stringText.toLowerCase();
 };
+
+palindromes('A car, a man, a maraca.')
 
 // Do not edit below this line
 module.exports = palindromes;
