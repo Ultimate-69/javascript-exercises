d11a56efbc7abb8a92614a6270b441543c8bc739
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-11-24 16:22:30 +0200

    reverseString solution complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(stringText) {
 
+    let reversedText = ''
+
+    for (i = stringText.length - 1; i >= 0; i--) {
+        reversedText += stringText[i];
+    }
+
+    return reversedText;
 };
 
 // Do not edit below this line
