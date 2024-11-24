6cdf43520aa100bdd3b013fdb3f552906cbf47f1
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-11-24 16:48:51 +0200

    sumAll solution complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2))
+    {
+        return 'ERROR';
+    }
+    let sum = num1 + num2;
+    if (num1 < num2)
+    {
+        for (i = num1 + 1; i < num2; i++) {
+            sum += i;
+            console.log(sum);
+        }
+    }
+    else {
+        for (i = num2 + 1; i < num1; i++) {
+            sum += i;
+            console.log(sum);
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
