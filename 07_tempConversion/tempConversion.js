d01babb0a20871d11a104bff5dfdabf126b304c2
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-11-24 17:03:56 +0200

    tempConversion solution complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let newTemperature = Number((((temperature - 32) * (5/9))).toFixed(1));
+  return newTemperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let newTemperature = Number(((temperature * (9/5) + 32)).toFixed(1));
+  return newTemperature;
 };
 
 // Do not edit below this line
