d8df2e0c5c5f3ccd21b883f919f50e8e37d5cc11
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-11-24 18:17:24 +0200

    fibonacci solution complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibNum = 0;
+    let otherNum = 1;
 
+    if (num < 0) {
+        return 'OOPS'
+    }
+
+    for (i = 0; i < num; i++) {
+        let result = fibNum + otherNum;
+        otherNum = fibNum
+        fibNum = result;
+    }
+
+    return fibNum;
 };
 
 // Do not edit below this line
